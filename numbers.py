3b022267f34b4cf7d7edd9383b96045cb4c02158
Niana <118477737+Nianalmao@users.noreply.github.com>
2022-12-02 15:08:06 +0100

    first commit

---
+++ numbers.py
@@ -0,0 +1,23 @@
+x = int(input("x="))
+while not (0<=x<=9):
+    x = int(input("x="))
+if (x==0):
+    print("zero")
+elif (x==1):
+    print("un")
+elif (x==2):
+    print("deux")
+elif (x==3):
+    print("trois")
+elif (x==4):
+    print("quatre")
+elif (x==5):
+    print("cinq")
+elif (x==6):
+    print("six")
+elif (x==7):
+    print("sept")
+elif (x==8):
+    print("huit")
+else:
+    print("meuf")
